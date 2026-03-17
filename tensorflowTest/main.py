82230d35c8b3f004211b710047dd0f25822d3dff
LOUISKANTI\louis <louis.dubettier@ksh.edu>
2026-03-17 19:06:46 +0100

    pemiere version

---
+++ tensorflowTest/main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import tensorflow as tf
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("tf version: ", tf.__version__)
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
